279d308d9b75989bcd36ee610815f55ffbea5da3
Chris Young and trevor elvey <ctopheryoung@gmail.com>
2016-02-10 10:48:42 -0800

    fix type in resources directory name and spark now functions

--- RPS/src/main/java/RPS.java
+++ RPS/src/main/java/RPS.java
@@ -1,11 +1,16 @@
 import java.util.Map;
 import java.util.HashMap;
 import spark.ModelAndView;
-// import spark.template.velocity.VelocityTemplateEngine;
+import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class RPS {
   public static void main(String[] args) {
-    
+
+    get("/", (request, response) -> {
+      HashMap model = new HashMap();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, "templates/layout.vtl");
+    }, new VelocityTemplateEngine());
   }
 }
--- RPS/src/test/java/AppTest.java
+++ RPS/src/test/java/AppTest.java
@@ -18,6 +18,6 @@ public class AppTest extends FluentTest {
   @Test
   public void rootTest() {
       goTo("http://localhost:4567/");
-      assertThat(pageSource()).contains("Leap year detector");
+      assertThat(pageSource()).contains("Rock, Paper, Scissors!");
   }
 }
