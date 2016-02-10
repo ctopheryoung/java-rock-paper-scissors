3ff5d334ec42404be33397ed63a3e7abc48012b1
Chris Young and trevor elvey <ctopheryoung@gmail.com>
2016-02-10 08:47:11 -0800

    initiate files

---
+++ RPS/src/test/java/RPSTest.java
@@ -0,0 +1,23 @@
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RPSTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+  public WebDriver getDefaultDriver() {
+      return webDriver;
+  }
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  @Test
+  public void checkWinner_rockBeatsScissors_true() {
+    RPS testRPS = new RPS();
+    assertEquals(true, testRPS.checkWinner("Rock", "Scissors"));
+  }
+}
