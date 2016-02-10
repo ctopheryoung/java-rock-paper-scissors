a24ba1dbd36bf44f0e1d0ae691885333f26b54c5
Chris Young and trevor elvey <ctopheryoung@gmail.com>
2016-02-10 11:16:22 -0800

    pass first spec rock beats scissors for real

--- RPS/src/test/java/UnitTest.java
+++ RPS/src/test/java/UnitTest.java
@@ -6,6 +6,22 @@ public class UnitTest {
   @Test
   public void checkWinner_rockBeatsScissors_true() {
   RPS testRPS = new RPS();
+  assertEquals(true, testRPS.checkWinner("Rock", "Scissors"));
+  }
+
+  @Test
+  public void checkWinner_rockBeatsScissors_true() {
+  RPS testRPS = new RPS();
   assertEquals(false, testRPS.checkWinner("Rock", "Scissors"));
   }
+
+  @Test
+  public void checkWinner_rockBeatsScissors_true() {
+  RPS testRPS = new RPS();
+  assertEquals(false, testRPS.checkWinner("Rock", "Scissors"));
+  }
+
+
+
+
 }
