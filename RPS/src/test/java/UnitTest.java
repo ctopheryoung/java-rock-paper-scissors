6472dcf82d5e18e7f7e346f7ab660314039b207a
Chris Young and trevor elvey <ctopheryoung@gmail.com>
2016-02-10 16:16:37 -0800

    fix tests

--- RPS/src/test/java/UnitTest.java
+++ RPS/src/test/java/UnitTest.java
@@ -4,40 +4,40 @@ import static org.junit.Assert.*;
 public class UnitTest {
 
   @Test
-  public void checkWinner_rockBeatsScissors_true() {
+  public void checkWinner_rockBeatsScissors_won() {
     RPS testRPS = new RPS();
-    assertEquals(true, testRPS.checkWinner("Rock", "Scissors"));
+    assertEquals("You won!", testRPS.checkWinner("Rock", "Scissors"));
   }
 
   @Test
-  public void checkWinner_paperBeatsRock_true() {
+  public void checkWinner_paperBeatsRock_won() {
     RPS testRPS = new RPS();
-    assertEquals(true, testRPS.checkWinner("Paper", "Rock"));
+    assertEquals("You won!", testRPS.checkWinner("Paper", "Rock"));
   }
 
   @Test
-  public void checkWinner_scissorsBeatsPaper_true() {
+  public void checkWinner_scissorsBeatsPaper_won() {
     RPS testRPS = new RPS();
-    assertEquals(true, testRPS.checkWinner("Scissors", "Paper"));
+    assertEquals("You won!", testRPS.checkWinner("Scissors", "Paper"));
   }
 
   @Test
-  public void checkWinner_rockLosesToPaper_false() {
+  public void checkWinner_rockLosesToPaper_lost() {
     RPS testRPS = new RPS();
-    assertEquals(false, testRPS.checkWinner("Rock", "Paper"));
+    assertEquals("You lost!", testRPS.checkWinner("Rock", "Paper"));
   }
 
-  @Test
-  public void checkTie_rockTiesRock_true() {
-    RPS testRPS = new RPS();
-    assertEquals(true, testRPS.checkTie("Rock", "Rock"));
-  }
-
-  @Test
-  public void checkTie_rockDontTiePaper_false() {
-    RPS testRPS = new RPS();
-    assertEquals(false, testRPS.checkTie("Rock", "Paper"));
-  }
+  // @Test
+  // public void checkTie_rockTiesRock_true() {
+  //   RPS testRPS = new RPS();
+  //   assertEquals(true, testRPS.checkTie("Rock", "Rock"));
+  // }
+  //
+  // @Test
+  // public void checkTie_rockDontTiePaper_false() {
+  //   RPS testRPS = new RPS();
+  //   assertEquals(false, testRPS.checkTie("Rock", "Paper"));
+  // }
 
   @Test
   public void computerPlay_shouldBeAString_true() {
